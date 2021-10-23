09175a8cf4ca1c8dae915c09b55c17ff73f5c7f6
anechaev <anechaev>
2021-10-23 14:00:47 +0300

    test

--- src/main/java/com/example/forabank1/controller/MainController.java
+++ src/main/java/com/example/forabank1/controller/MainController.java
@@ -8,6 +8,8 @@ import com.example.forabank1.repo.FastPaymentDataRepo;
 import com.example.forabank1.repo.OperationRepo;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,8 @@ public class MainController {
 
     private final FastPaymentDataRepo fastPaymentDataRepo;
     private final OperationRepo operationRepo;
+    @Autowired
+    Environment environment;
 
     public MainController(FastPaymentDataRepo fastPaymentDataRepo, OperationRepo operationRepo) {
         this.fastPaymentDataRepo = fastPaymentDataRepo;
@@ -47,5 +51,14 @@ public class MainController {
         operationRepo.deleteAll();
         List<Operation> operations = new Parser().findAndParse();
         operationRepo.saveAll(operations);
+    }
+
+
+    @RequestMapping(value = "/test")
+    public String main() {
+        String msg = "Your server is up and running at port:" + environment.getProperty("local.server.port") + "\n" +
+            "Address: " + environment.getProperty("local.server.host");
+
+        return msg;
     }
 }
